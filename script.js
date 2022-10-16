9d5249e887628e0943ce6078c38d5376ffbf6a92
John <johnreysecoya@gmail.com>
2022-10-16 12:18:16 +0800

    cleaning up code

--- script.js
+++ script.js
@@ -2,8 +2,35 @@
 
 const TO_CHOOSE = ["rock", "paper", "scissor"];
 
+let rockbtn = document.querySelector(".rock");
+let paperbtn = document.querySelector(".paper");
+let scissorbtn = document.querySelector(".scissor");
+
+let user_score = 0;
+let computer_score = 0;
+
+let user_choice = "";
+let computer_choice = "";
+let message_result = "";
+
+let user_name = "";
+
 // Compute Choice
-function computer_choice(choices) {
+
+// To return who is the winner of a single game
+
+function one_game_result(choice, to_choose) {
+  user_choice = choice;
+  computer_choice = computer_choice_f(to_choose);
+  let user_computer = [user_choice, computer_choice];
+  let [win_piece, win_message] = winning_piece(user_computer);
+  message_result = win_message;
+  let round_winner = who_won(win_piece, user_computer);
+
+  return round_winner;
+}
+
+function computer_choice_f(choices) {
   let random_index = Math.floor(Math.random() * choices.length);
   return choices[random_index];
 }
@@ -12,14 +39,14 @@ function computer_choice(choices) {
 function winning_piece(two_choice) {
   let [user, computer] = two_choice;
   if (user == computer) {
-    return "draw";
+    return ["draw", "No one wins"];
   } else {
     if (two_choice.includes("paper") && two_choice.includes("rock")) {
-      return ["paper", "paper beats rock"];
+      return ["paper", "Paper beats Rock!"];
     } else if (two_choice.includes("rock") && two_choice.includes("scissor")) {
-      return ["rock", "rock beats scissor"];
+      return ["rock", "Rock beats Scissor!"];
     } else {
-      return ["scissor", "scissor beats paper"];
+      return ["scissor", "Scissor beats Paper!"];
     }
   }
 }
@@ -28,23 +55,12 @@ function winning_piece(two_choice) {
 function who_won(win_piece, user_computer) {
   let [user, computer] = user_computer;
   if (win_piece == user) {
-    return "user";
+    return `${user_name}`;
   } else if (win_piece == computer) {
     return "computer";
   } else {
     return "draw";
   }
-}
-
-// To return who is the winner of a single game
-function one_game_result(choice, to_choose) {
-  let user = choice;
-  let computer = computer_choice(to_choose);
-  let user_computer = [user, computer];
-  let [win_piece, message] = winning_piece(user_computer);
-  let round_winner = who_won(win_piece, user_computer);
-
-  return round_winner;
 }
 
 // Check who is the grand winner of the game
@@ -72,8 +88,18 @@ function display_winner_text(winner) {
   }
 }
 
+function display_user_computer_choice() {
+  let user_para = document.querySelector(".user_choice_p");
+  let computer_para = document.querySelector(".computer_choice_p");
+  let message_para = document.querySelector(".result_message");
+
+  user_para.textContent = `${user_name} chooses ${user_choice}`;
+  computer_para.textContent = `Computer chooses ${computer_choice}`;
+  message_para.textContent = `${message_result}`;
+}
+
 function update_score(winner) {
-  if (winner == "user") {
+  if (winner == `${user_name}`) {
     user_score += 1;
   } else if (winner == "computer") {
     computer_score += 1;
@@ -83,7 +109,7 @@ function update_score(winner) {
 function update_text_score() {
   let user_score_text = document.querySelector(".user_score");
   let computer_score_text = document.querySelector(".computer_score");
-  user_score_text.textContent = `User Score: ${user_score}`;
+  user_score_text.textContent = `${user_name} Score: ${user_score}`;
   computer_score_text.textContent = `Computer Score: ${computer_score}`;
 }
 
@@ -93,18 +119,24 @@ function game(winner) {
   } else {
     update_score(winner);
     update_text_score();
+    display_user_computer_choice();
     display_winner_text(winner);
   }
 }
 
 // Game Start
 
-let rockbtn = document.querySelector(".rock");
-let paperbtn = document.querySelector(".paper");
-let scissorbtn = document.querySelector(".scissor");
+while (true) {
+  user_name = prompt("Enter your name: ");
+  if (user_name == "") {
+    alert("Name cannot be empty Try Again!");
+  } else {
+    break;
+  }
+}
 
-let user_score = 0;
-let computer_score = 0;
+let user_score_text = document.querySelector(".user_score");
+user_score_text.textContent = `${user_name} Score: ${user_score}`;
 
 // Event Listener
 
