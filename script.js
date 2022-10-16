0cc9beb64728732c47d186e182355fbaa816ae65
John <johnreysecoya@gmail.com>
2022-10-16 08:32:38 +0800

    Initial branch commit

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 "use strict";
 
-let available_choice = ["rock", "paper", "scissor"];
+const TO_CHOOSE = ["rock", "paper", "scissor"];
 
 // General Function
 
 // Computer and User Choice
+
+// Get Computer Choice in the choices/availble choices array
 function computer_choice(choices) {
   let random_index = Math.floor(Math.random() * choices.length);
   return choices[random_index];
-}
-
-function user_choice(choices) {
-  let choice = prompt("Choose rock/papaer/scissors").toLowerCase();
-  while (true)
-    if (is_valid(choice, choices)) {
-      break;
-    } else {
-      choice = prompt(
-        "Please Try again! Choose rock/papaer/scissors"
-      ).toLowerCase();
-    }
-  return choice;
 }
 
 // Display functions
@@ -62,7 +51,7 @@ function is_valid(user_choice, choices) {
 
 // main functions
 
-// Check which of the two piece wins ie, paper vs rock etc
+// Check which of the two piece wins, ie, paper vs rock etc
 function winning_piece(two_choice) {
   let [user, computer] = two_choice;
   if (user == computer) {
@@ -78,7 +67,7 @@ function winning_piece(two_choice) {
   }
 }
 
-// to check which user holds the winning piece
+// Check which user holds the winning piece
 function who_won(win_piece, user_computer) {
   let [user, computer] = user_computer;
   if (win_piece == user) {
@@ -91,9 +80,9 @@ function who_won(win_piece, user_computer) {
 }
 
 // To return who is the winner of a single game
-function one_game_result(choices) {
-  let user = user_choice(choices);
-  let computer = computer_choice(choices);
+function one_game_result(choice, to_choose) {
+  let user = choice;
+  let computer = computer_choice(to_choose);
   let user_computer = [user, computer];
   let [win_piece, message] = winning_piece(user_computer);
   let round_winner = who_won(win_piece, user_computer);
@@ -103,6 +92,7 @@ function one_game_result(choices) {
   return round_winner;
 }
 
+// Check who is the grand winner of the game
 function grand_winner(user_score) {
   if (user_score == 5) {
     return "User";
@@ -111,6 +101,7 @@ function grand_winner(user_score) {
   }
 }
 
+// The game loop, loop until someone reaches a score of 5
 function game_loop() {
   let user_score = 0;
   let computer_score = 0;
@@ -127,4 +118,74 @@ function game_loop() {
   display_grand_winner(user_score);
 }
 
-game_loop();
+function display_winner_text(winner) {
+  let p_winner = document.querySelector(".winner_text");
+  if (winner != "draw") {
+    p_winner.textContent = `The winner is ${winner}`;
+  } else {
+    p_winner.textContent = "It's a draw!";
+  }
+  p_winner.classList.remove("hidden");
+
+  if (user_score == 5) {
+    p_winner.textContent = `Congratulations! You are the Grand Champion!`;
+  } else if (computer_score == 5) {
+    p_winner.textContent = `Sorry you lose. Computer is the Grand Champion!`;
+  }
+}
+
+function update_score(winner) {
+  if (winner == "user") {
+    user_score += 1;
+  } else if (winner == "computer") {
+    computer_score += 1;
+  }
+}
+
+function update_text_score() {
+  let user_score_text = document.querySelector(".user_score");
+  let computer_score_text = document.querySelector(".computer_score");
+  user_score_text.textContent = `User Score: ${user_score}`;
+  computer_score_text.textContent = `Computer Score: ${computer_score}`;
+}
+
+// // game_loop();
+let rockbtn = document.querySelector(".rock");
+let paperbtn = document.querySelector(".paper");
+let scissorbtn = document.querySelector(".scissor");
+
+let user_score = 0;
+let computer_score = 0;
+
+rockbtn.addEventListener("click", () => {
+  let winner = one_game_result(rockbtn.textContent, TO_CHOOSE);
+  if (user_score == 5 || computer_score == 5) {
+    alert("Game is over");
+  } else {
+    update_score(winner);
+    update_text_score();
+    display_winner_text(winner);
+  }
+});
+
+paperbtn.addEventListener("click", () => {
+  let winner = one_game_result(paperbtn.textContent, TO_CHOOSE);
+  if (user_score == 5 || computer_score == 5) {
+    alert("Game is over");
+  } else {
+    update_score(winner);
+    update_text_score();
+    display_winner_text(winner);
+  }
+});
+
+scissorbtn.addEventListener("click", () => {
+  let winner = one_game_result(scissorbtn.textContent, TO_CHOOSE);
+  if (user_score == 5 || computer_score == 5) {
+    alert("Game is over");
+  } else {
+    update_score(winner);
+    update_text_score();
+    display_winner_text(winner);
+  }
+});
